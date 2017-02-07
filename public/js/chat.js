a90ec4bed7248b5ca5734b5cea78dab0347bfbc0
matteoredaelli <matteo.redaelli@gmail.com>
2017-02-07 22:15:01 +0100

    first import

---
+++ public/js/chat.js
@@ -0,0 +1,28 @@
+var app = angular.module('myApp', ['ngSanitize']);
+
+app.config(['$httpProvider', function($httpProvider) {
+        //$httpProvider.defaults.useXDomain = true;
+        //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+app.controller('planetController', function($scope, $http, $location) {
+  $scope.searchR = function(query) {
+    var url = $location.protocol() + "://" + location.host + "/chateo/reply";
+    var payload = {
+      message: query,
+      //vars:{name:""}};
+      vars:{}};
+    $http.post(url, payload, {
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(function(response) {
+        ga('send', 'event', 'chaTeo', 'question', query.toLowerCase());
+        
+	$scope.myreply = response.data.reply;
+	//$scope.history = response.data.vars["__history__"];
+	$scope.query = "";
+      });
+  };
+  $scope.myreply = "Tell me something, I'll try to answer you...";
+});
